57315384b7fe6774332b02d0f608b3a818e25cb8
JaRodriguez97 <cristi542012@gmail.com>
2024-03-27 14:13:38 -0500

    testimonios

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -4,7 +4,9 @@ import {
   faFacebookF,
   faInstagram,
   faGoogle,
+  faTiktok,
 } from '@fortawesome/free-brands-svg-icons';
+// import moduleName from '@fortawesome/free';
 
 @Component({
   selector: 'app-index',
@@ -15,6 +17,7 @@ export class IndexComponent implements OnInit {
   faFacebookF = faFacebookF;
   faInstagram = faInstagram;
   faGoogle = faGoogle;
+  faTiktok = faTiktok;
 
   constructor() {} // public publicService: PublicService
   ngOnInit() {}
