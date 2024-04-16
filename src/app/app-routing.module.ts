348c79137cbade6112cdf0240a5fd89a541331c3
JaRodriguez97 <cristi542012@gmail.com>
2024-04-16 17:27:55 -0500

    SEO 8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'landing', pathMatch: 'full' },
   {
-    path: 'landing',
+    path: '',
+    redirectTo: 'servicio-grua-cali-economicas-24-horas',
+    pathMatch: 'full',
+  },
+  {
+    path: 'servicio-grua-cali-economicas-24-horas',
     loadChildren: () =>
       import('./modules/Landing/landing.module').then((m) => m.LandingModule),
     title: 'Servicio Grúa Cali Economicas 24 Horas - Grúas CL',
