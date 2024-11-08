2d71defbd21bd7a94a3a476b690d287151d8b39b
JaRodriguez97 <cristi542012@gmail.com>
2024-11-08 10:25:32 -0500

    actualizacion logo y colores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'servicio-grua-cali-economicas-24-horas',
-    pathMatch: 'full',
-  },
-  {
     path: 'servicio-grua-cali-economicas-24-horas',
     loadChildren: () =>
       import('./modules/Landing/landing.module').then((m) => m.LandingModule),
@@ -15,11 +10,7 @@ const routes: Routes = [
   },
   {
     path: '**',
-    loadChildren: () =>
-      import('./modules/not-found/not-found.module').then(
-        (m) => m.NotFoundModule
-      ),
-    title: 'Servicio Grúa Cali Economicas 24 Horas - Grúas CL',
+    redirectTo: 'servicio-grua-cali-economicas-24-horas',
   },
 ];
 
